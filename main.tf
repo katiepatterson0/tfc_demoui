c62afe5436cefa3f81072a2321016d2cfa82e51f
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-03 10:25:57 -0400

    Update main.tf
    updated for no code with provider details

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0.1, <5.43.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "my_new_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
@@ -7,4 +17,3 @@ resource "aws_instance" "my_new_app_server" {
     Name = "Example EC2 for demo"
   }
 }
-
