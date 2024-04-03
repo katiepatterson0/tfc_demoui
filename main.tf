3b3e8419164b7773003ece2b5a9e922c8dc22d4a
Patterson Krise <patterson@hashicorp.com>
2024-04-03 15:03:45 -0400

    commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_new_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
-  instance_type = "t2.micro"
+  instance_type = "t1.micro"
 
   tags = {
     Name = "Example EC2 for demo"
