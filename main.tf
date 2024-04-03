b83b9603b35b69a52cf65ed0362514ae693b2047
Patterson Krise <patterson@hashicorp.com>
2024-04-03 13:48:05 -0400

    Commit

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=5.0.1, <5.43.0"
-    }
-  }
-}
-provider "aws" {
-  region = "us-east-1"
-}
 resource "aws_instance" "my_new_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws-access-key" {
+    type = string
+    description = "aws access key"
+
+}
+
+variable "aws-secret-access-key" {
+    type = string
+    description = "aws secret access key"
+}
