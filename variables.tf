30fe4725fddf52fc6e9eab54f5101d681249407b
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 14:52:47 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,7 @@ variable "aws-secret-access-key" {
     type = string
     description = "aws secret access key"
 }
+variable "image_id" {
+    type = string
+    description = "ID of the image from a security perspective."
+}
