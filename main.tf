bc5abbc25f1a6ec6b1dad12f178453ce9bd8e897
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 16:49:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "my_new_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
 
-  tags = {
+  tags_all = {
     Name = var.instance_name
+    Created_by = "Patterson"
   }
-}
+  {
+
