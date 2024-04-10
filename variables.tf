db44115b19d7ab867cc5e3121f0f8c6b43f6cf83
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 15:06:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,6 @@ variable "image_id" {
     type = string
     description = "ID of the image from a security perspective."
 }
+output "ec2_private_ip" {
+    value = aws_instance.server.private_ip
+}
