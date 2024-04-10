a953532cb0054c8a8e53e36d52e4ffdf4e5f969e
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 15:08:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,6 +12,5 @@ variable "image_id" {
     type = string
     description = "ID of the image from a security perspective."
 }
-output "ec2_private_ip" {
-    value = aws_instance.server.private_ip
-}
+output "image_id"
+    value = 
