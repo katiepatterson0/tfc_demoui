6e3cbcf9ffcb1c0fd48d6de1c776b0805e52d452
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 17:32:46 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 #outputs.tf
 
 output "ec2_name" {
- description = "Name of EC2 instance"    
-value = var.ec2_name
+   description = "Name of EC2 instance"    
+   value = var.ec2_name
 }
 
 output "ec2_public_ip" {
-    description 
+   description = "Public IP of the EC2"
+   value = aws_instance.
