29c3c985c06fd5bbe1ccdc99d21661a1a24c32c9
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 17:35:59 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "ec2_public_ip" {
    description = "Public IP of the EC2"
    value = aws_instance.my_new_app_server.public_ip
 }
+
+output "ec2_private_ip" {
+   description = "Private IP of the EC2"
+   value = aws_instance.my_new_app_server.private_ip
+}
