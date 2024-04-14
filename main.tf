4485652da85d6c12a7078b15bc41c0c46da45d97
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-14 12:51:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_new_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
-  instance_type = "t1.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = var.ec2_name
